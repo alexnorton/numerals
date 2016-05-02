02cf9c89c8817cd2cbf78b66df6a60169758b184
Alex Norton <sandig@gmail.com>
2016-05-02 20:56:26 +0100

    initial test for function presence

--- spec/numerals_spec.js
+++ spec/numerals_spec.js
@@ -1,1 +1,15 @@
-var numerals = require("../numerals")
+describe("numerals", function() {
+  var numerals = require("../numerals");
+
+  describe(".generate", function() {
+    it("is exported", function() {
+      expect(typeof numerals.generate).toBe("function");
+    });
+  });
+
+  describe(".parse", function() {
+    it("is exported", function() {
+      expect(typeof numerals.generate).toBe("function");
+    });
+  });
+});
